7c1c4aaafa890c64ec076772da0a0c3a81a49d72
Karl <kacue38@gmail.com>
2022-04-01 19:56:55 -0700

    added css style and a place holder for cloning divs

--- src/app.js
+++ src/app.js
@@ -1,12 +1,11 @@
 const originalOneSection = document.querySelector('section.originals div.original-container')
 const copiesSection = document.querySelector('div.copy-container')
 
-const originalOneTemplate = `  <div><p>one</p></div>`
-const originalTwoTemplate = `  <div><p>two</p></div>`
+const originalOneTemplate = `  <div><p>one</p></div>` //place holder if i dont figure it out
 
 function addTemplateOne() {
     originalsection = document.querySelector('.div.original-container')
-    copiesSection.insertAdjacentHTML('afterbegin', 'test')
+    copiesSection.insertAdjacentHTML('afterbegin', originalOneTemplate)
     console.log(originalsection)
 }
 
